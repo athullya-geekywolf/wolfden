33da16a3487023943024fa3591cd737e6d102386
athullya.r <athullya.r@CQLAP-52>
2024-12-04 14:32:39 +0530

    cron job to do cases

--- src/services/WolfDen.Application/Requests/Queries/Attendence/DailyDetails/DailyDetailsQueryHandler.cs
+++ src/services/WolfDen.Application/Requests/Queries/Attendence/DailyDetails/DailyDetailsQueryHandler.cs
@@ -1,6 +1,6 @@
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using WolfDen.Application.Requests.DTOs.Attendence;
 using WolfDen.Domain.ConfigurationModel;
 using WolfDen.Domain.Entity;
@@ -12,8 +12,8 @@ namespace WolfDen.Application.Requests.Queries.Attendence.DailyStatus
     public class DailyDetailsQueryHandler :IRequestHandler<DailyDetailsQuery,DailyAttendanceDTO>
     {
         private readonly WolfDenContext _context;
-        private readonly OfficeDurationSettings _officeDurationSettings ;
-        public DailyDetailsQueryHandler(WolfDenContext context, OfficeDurationSettings officeDurationSettings)
+        private readonly IOptions<OfficeDurationSettings> _officeDurationSettings;
+        public DailyDetailsQueryHandler(WolfDenContext context, IOptions<OfficeDurationSettings> officeDurationSettings)
         {
             _context = context;
             _officeDurationSettings = officeDurationSettings;
@@ -22,7 +22,6 @@ namespace WolfDen.Application.Requests.Queries.Attendence.DailyStatus
         {
             DateOnly currentDate=request.Date;
             DateOnly today = DateOnly.FromDateTime(DateTime.UtcNow);
-            
             if(currentDate==today)
             {
                 DailyAttendanceDTO holiday = new DailyAttendanceDTO();
@@ -35,17 +34,17 @@ namespace WolfDen.Application.Requests.Queries.Attendence.DailyStatus
                 holiday.AttendanceStatusId=AttendanceStatus.Weekend;
                 return holiday;    
             }
-            int minWorkDuration = _officeDurationSettings.MinWorkDuration;
+            int minWorkDuration = _officeDurationSettings.Value.MinWorkDuration;
             DailyAttendanceDTO? attendence = await _context.DailyAttendence
                 .Where(x => x.EmployeeId == request.EmployeeId && x.Date == request.Date)
                 .Select(x => new DailyAttendanceDTO
-                {
-                    ArrivalTime = x.ArrivalTime,
-                    DepartureTime = x.DepartureTime,
-                    InsideHours = x.InsideDuration,
-                    OutsideHours = x.OutsideDuration,
-                    MissedPunch = x.MissedPunch,
-                 }).FirstOrDefaultAsync(cancellationToken);
+            {
+                ArrivalTime = x.ArrivalTime,
+                DepartureTime = x.DepartureTime,
+                InsideHours = x.InsideDuration,
+                OutsideHours = x.OutsideDuration,
+                MissedPunch = x.MissedPunch,
+            }).FirstOrDefaultAsync(cancellationToken);
 
             if (attendence is null)
             {
@@ -84,26 +83,12 @@ namespace WolfDen.Application.Requests.Queries.Attendence.DailyStatus
                             ? AttendanceStatus.WFH : AttendanceStatus.Leave;
                     }
                 }
-
                 return notPresentDay;
             }
             else
             {
-                LeaveRequest? leave = await _context.LeaveRequests
-                       .Where(x => x.EmployeeId == request.EmployeeId && x.FromDate <= request.Date && request.Date <= x.ToDate && x.LeaveRequestStatusId == LeaveRequestStatus.Approved)
-                       .Include(x => x.LeaveType)
-                       .FirstOrDefaultAsync(cancellationToken);
-                if(leave is not null && leave.HalfDay is true)
-                {
-                    minWorkDuration = minWorkDuration / 2;
-                    attendence.AttendanceStatusId= (attendence.InsideHours >= minWorkDuration) ?
-                    AttendanceStatus.HalfDayLeave : AttendanceStatus.IncompleteShift;
-                }
-                else
-                {
-                    attendence.AttendanceStatusId = (attendence.InsideHours >= minWorkDuration) ?
+                attendence.AttendanceStatusId = (attendence.InsideHours >= minWorkDuration) ?
                     AttendanceStatus.Present : AttendanceStatus.IncompleteShift;
-                }
             }
             List<AttendenceLogDTO> attendenceRecords = await _context.AttendenceLog
                 .Where(x => x.EmployeeId == request.EmployeeId && x.PunchDate == request.Date)
